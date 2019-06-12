6b13c50e7c9a17f8218c91a71b55aa9ba9fe43ab
Yusuf Nar <yusufnar@gmail.com>
2019-06-12 21:29:30 +0300

    qpress required agent info

--- cookbooks/eydr/recipes/install_xtrabackup.rb
+++ cookbooks/eydr/recipes/install_xtrabackup.rb
@@ -27,7 +27,7 @@ end
 bash "download-qpress" do
   user node['owner_name']
   cwd "/home/#{node['owner_name']}/"
-  code "wget #{node[:dr_replication][:qpress_download_url]}"
+  code "wget -U 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)' #{node[:dr_replication][:qpress_download_url]}"
   not_if { File.exists? "/home/#{node['owner_name']}/#{node[:dr_replication][:qpress_download_url].split("/").last}"}
 end
 
